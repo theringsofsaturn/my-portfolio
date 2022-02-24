e04284339b8f06e1e724ea2e93785050fa41287d
theringsofsaturn <emiliankasemi@gmail.com>
2022-02-24 03:33:02 +0100

    imported all components to App

---
+++ frontend/src/App.jsx
@@ -0,0 +1,9 @@
+import { About, Footer, Header, Skills, Testimonial, Work } from "./container";
+
+const App = () => {
+  return (
+   <div className="app">Hello</div>
+  )
+};
+
+export default App;
