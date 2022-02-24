893e07fb2871d19f532cd83ea324569c82265b52
theringsofsaturn <emiliankasemi@gmail.com>
2022-02-24 03:40:04 +0100

    imported and structured all the components in the App

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,18 @@
 import { About, Footer, Header, Skills, Testimonial, Work } from "./container";
+import { Navbar } from "./components";
 
 const App = () => {
   return (
-   <div className="app">Hello</div>
-  )
+    <div className="app">
+      <Navbar />
+      <Header />
+      <About />
+      <Work />
+      <Skills />
+      <Testimonial />
+      <Footer />
+    </div>
+  );
 };
 
 export default App;
