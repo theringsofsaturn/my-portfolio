e1bc531a5be151d223bbeb61eba8547a0f2d2bb6
theringsofsaturn <emiliankasemi@gmail.com>
2022-02-26 00:02:21 +0100

    added classnames according to BEM methodology

--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -3,14 +3,15 @@ import "./Navbar.scss";
 
 const Navbar = () => {
   return (
-    <nav>
-      <div>
+    // Classnames according to BEM methodology
+    <nav className="app__navbar">
+      <div className="app__navbar-logo">
         <img src={images.logo} alt="logo" />
       </div>
-      <ul>
+      <ul className="app__navbar-links">
         {["home", "about", "contact", "work", "skills", "contact"].map(
           (item) => (
-            <li key={`link-${item}`}>
+            <li className="app__flex p-text" key={`link-${item}`}>
               <div />
               <a href={`#${item}`}>{item}</a>
             </li>
