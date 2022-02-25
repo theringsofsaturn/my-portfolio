5a5ab2d82bd25b3d6642ed4e1e80bb217be3e5c2
theringsofsaturn <emiliankasemi@gmail.com>
2022-02-25 23:52:04 +0100

    created and mapping for the navbar links

--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -7,6 +7,16 @@ const Navbar = () => {
       <div>
         <img src={images.logo} alt="logo" />
       </div>
+      <ul>
+        {["home", "about", "contact", "work", "skills", "contact"].map(
+          (item) => (
+            <li key={`link-${item}`}>
+              <div />
+              <a href={`#${item}`}>{item}</a>
+            </li>
+          )
+        )}
+      </ul>
     </nav>
   );
 };
