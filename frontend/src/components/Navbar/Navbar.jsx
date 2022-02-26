810e2c33ceae747e28bac0c7a3265b89ad43f605
theringsofsaturn <emiliankasemi@gmail.com>
2022-02-26 01:38:01 +0100

    Toggle condition onClick for the navbar on small devices

--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -1,7 +1,14 @@
 import { images } from "../../constants";
+
+import { HiMenuAlt4, HiX, Hix } from "react-icons/hi";
+import { motion } from "framer-motion"; // Framer motion
+
 import "./Navbar.scss";
+import { useState } from "react";
 
 const Navbar = () => {
+  const [toggle, setToggle] = useState(false);
+
   return (
     // Classnames according to BEM methodology
     <nav className="app__navbar">
@@ -18,6 +25,33 @@ const Navbar = () => {
           )
         )}
       </ul>
+
+      <div className="app__navbar-menu">
+        <HiMenuAlt4 onClick={() => setToggle(true)} />
+
+        {
+          // If toggle true then render the Mobile navigation bar (when we are on small screens)
+          toggle && (
+            // Special div from framer-motion. check /www.framer.com
+            <motion.div
+              whileInView={{ x: [300, 0] }} // it will take 300px
+              transition={{ duration: 0.85, ease: "easeOut" }}
+            >
+              <HiX onClick={() => setToggle(false)} />
+              {["home", "about", "contact", "work", "skills", "contact"].map(
+                (item) => (
+                  <li key={{ item }}>
+                    {/* The anchor tag also has an onClick so when we click a link inside the navigation bar we want to close the navigation and redirect to the section we clicked and see that instead */}
+                    <a href={`#${item}`} onClick={() => setToggle(false)}>
+                      {item}
+                    </a>
+                  </li>
+                )
+              )}
+            </motion.div>
+          )
+        }
+      </div>
     </nav>
   );
 };
