15822e8ee1a08df3d4ace241a4aab4a1909cfa22
theringsofsaturn <emiliankasemi@gmail.com>
2022-02-27 17:11:46 +0100

    Skills component

--- frontend/src/container/Work/Work.jsx
+++ frontend/src/container/Work/Work.jsx
@@ -42,7 +42,7 @@ const Work = () => {
       </h2>
       <div className="app__work-filter">
         <div className="app__work-filter">
-          {["UI/UX", "Web App", "Mobile App", "React JS", "All"].map(
+          {["Web App", "Mobile App", "Games", "All"].map(
             (item, index) => (
               <div
                 key={index}
