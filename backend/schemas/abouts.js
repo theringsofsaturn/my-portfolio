8f43d9689adf65c9a510f5e152f2285affa0b573
theringsofsaturn <emiliankasemi@gmail.com>
2022-02-27 02:16:34 +0100

    configuration code in schema for custom sort documents

--- backend/schemas/abouts.js
+++ backend/schemas/abouts.js
@@ -1,26 +1,40 @@
-export default{
-    name:'abouts',
-    title:'Abouts',
-    type: 'document',
-    fields:[
-        {
-            name:'title',
-            title:'Title',
-            type:'string'
-        },
-        {
-            name:'description',
-            title:'Description',
-            type:'string'
-        },
-        {
-            name:'imgUrl',
-            title:'ImgUrl',
-            type: 'image',
-            options: {
-              hotspot: true,
-            },
-        },
-        
-    ]
-}
+export default {
+  name: "abouts",
+  title: "Abouts",
+  type: "document",
+  fields: [
+    {
+      name: "title",
+      title: "Title",
+      type: "string",
+    },
+    {
+      name: "description",
+      title: "Description",
+      type: "string",
+    },
+    {
+      name: "imgUrl",
+      title: "ImgUrl",
+      type: "image",
+      options: {
+        hotspot: true,
+      },
+    },
+
+    {
+        name: "order",
+        title: "Order",
+        type: "number",
+        hidden: true,
+      },
+  ],
+
+  orderings: [
+    {
+      title: "Manual order",
+      name: "manualOrder",
+      by: [{ field: "order", direction: "asc" }],
+    },
+  ],
+};
