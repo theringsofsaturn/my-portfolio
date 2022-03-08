e6ec639418fe6e8e7c31afd250e28cf01673009d
theringsofsaturn <emiliankasemi@gmail.com>
2022-03-08 23:53:35 +0100

    minor text change

--- frontend/src/container/Header/Header.jsx
+++ frontend/src/container/Header/Header.jsx
@@ -27,7 +27,7 @@ const Header = () => {
           <div className="badge-cmp app__flex">
             <span>👋</span>
             <div style={{ marginLeft: 20 }}>
-              <p className="p-text">Hello, I am</p>
+              <p className="p-text">I am</p>
               <h1 className="head-text">Emilian</h1>
             </div>
           </div>
